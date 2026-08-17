b862fa70a87ba2268703529c81cb130a1d9a794d
ted3xx-sys <ted3xx@gmail.com>
2026-08-17 01:51:14 -0500

    Polish bills layout and app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Bump this when shipping new assets so old caches (icons, html, etc.) are evicted.
-const CACHE = 'ghp-v7';
+const CACHE = 'ghp-v8';
 
 self.addEventListener('install', e => {
   e.waitUntil(
